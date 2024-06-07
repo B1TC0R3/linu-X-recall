cdfe9056704d84d91889d3b571e006d476a9dbfa
Felix Schladt <felix.schladt@gmail.com>
2024-06-07 11:07:48 +0200

    [DEV] add screenshot feature

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,52 @@
 pub mod x11_windows;
+<<<<<<< HEAD
+=======
+pub mod screenshot;
+>>>>>>> 63147e8 ([DEV] add screenshot feature)
 
 use x11_windows::{Window, get_windows};
+use screenshot::{screenshot_full};
+
+use std::{
+    path::{Path},
+    process::exit,
+    fs,
+};
+
+const LOG_DIR: &str = "var/log/recall";
+
+/*
+struct Context {
+    logging_folder: Path,
+    day_folder: Path,
+    hour_folder: Path,
+}*/
+
+// Screenshots -> Only if not locked
+// Deamon
+// Bash History
+// Browser History
+// Browser cookies
+// Keylogger
+
+
+
+fn init_logdir() { 
+    if !Path::new(LOG_DIR).exists() {
+        match fs::create_dir_all(LOG_DIR) {
+            Ok(_) => { return; }
+            Err(e) => {
+                eprintln!("Error creating recall logdir '{}': {}", LOG_DIR, e);
+                exit(-1);
+            }
+        }
+    }
+}
+
 
 fn main() {
+    init_logdir();
+
     let windows: Vec<Window> = match get_windows() {
         Ok(windows) => windows,
         Err(msg) => {
@@ -11,7 +55,6 @@ fn main() {
         }
     };
 
-    for win in windows {
-        println!("Time: {}, ID: {}, Title: {}", win.timestamp, win.id, win.title);
-    }
+    screenshot_full(Path::new(LOG_DIR));
+
 }
---
+++ src/screenshot.rs
@@ -0,0 +1,29 @@
+use xcap::Monitor;
+use std::path::Path;
+use anyhow::{Result, Context};
+use chrono::Local; 
+
+
+fn normalized(filename: &str) -> String {
+    filename
+        .replace("|", "")
+        .replace("\\", "")
+        .replace(":", "")
+        .replace("/", "")
+}
+
+pub fn screenshot_full(log_dir: &Path)  -> Result<()> {
+    let timestamp_formatted = Local::now().format("%Y-%m-%d-%H:%m:%S");
+    
+
+    for monitor in Monitor::all()? {
+        let screenshot_name = format!("screenshot-{}-{}.png", timestamp_formatted, normalized(monitor.name()));
+        let screenshot_path = log_dir.join(screenshot_name);
+        let screenshot = monitor.capture_image()?;
+        println!("Monitor Name: {}", monitor.name());
+        screenshot.save(screenshot_path)?;
+    }
+
+
+    Ok(())
+}
